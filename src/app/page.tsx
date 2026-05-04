42dca432516dc3069c0a022e5b77449574a9ad1e
Vincent <vincentkiathadi@gmail.com>
2026-05-05 05:19:26 +0800

    remove space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <HomeBanner/>
-        Home Page
+          Home Page
         <TopProducts/>
         <Testimonial/>
       </main>
