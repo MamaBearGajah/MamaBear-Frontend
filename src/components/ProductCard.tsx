a4b088fb1d70183e5bd17565f313fe8a3c14298e
Vincent <vincentkiathadi@gmail.com>
2026-05-08 08:27:33 +0800

    add: variant button click

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,17 @@
+"use client"
+
+import { useState } from "react";
 import { getProductId } from "../../server";
 import { mockProducts } from "../../lib/MockProducts";
 import Stars from "./Stars";
 
-export default async function ProductCard({
+export default function ProductCard({
   productId,
 }: {
   productId: number;
 })  {
-  const fetchedProduct = await getProductId(productId);
+  const [selected, setSelected] = useState<string | null>(null)
+  // const fetchedProduct = await getProductId(productId);
   return (
     <div className="bg-white border border-zinc-300 md:flex md:items-start rounded transition-transform duration-200 cursor-pointer hover:-translate-y-1 hover:shadow-lg">
         <div>
@@ -26,6 +30,23 @@ export default async function ProductCard({
             <p>In Stock</p>
             <p>{mockProducts[0].description}</p>
             <p>Flavour : <span className='text-[var(--mamabear-dark-pink)] font-bold'>{mockProducts[0].variants[0].value}</span></p>
+            <div className='mt-2 mb-2'>
+              {
+                mockProducts[0].variants.map((item,index) =>{
+                  return(
+                    <span 
+                    key={item.value} 
+                    onClick={()=>setSelected(item.value)} 
+                    className={
+                      `rounded-lg p-2 ml-2 border 
+                          ${selected===item.value ? "bg-[var(--mamabear-dark-pink)] text-white" : "bg-white text-black"}
+                      `}>
+                        {item.value}
+                    </span>
+                  )
+                })
+              }
+            </div>
             <div>Add To Cart</div>
         </div>
 
