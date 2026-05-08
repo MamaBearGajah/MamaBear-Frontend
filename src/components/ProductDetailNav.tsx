7d3b185f73c30ff9352799428df5b53cdddeacc8
Vincent <vincentkiathadi@gmail.com>
2026-05-09 07:57:50 +0800

    product detailnav

---
+++ src/components/ProductDetailNav.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function ProductDetailNav(){
+    return(
+    <div className='flex justify-start items-center gap-3'>
+        <p> Description</p>
+        <p> Ingredients</p>
+        <p>How To Use</p>
+        <p>Review</p>
+    </div>
+    )
+}
--- src/components/ReviewSection.tsx
+++ src/components/ReviewSection.tsx
@@ -1,10 +1,12 @@
 import ReviewCard from "./ReviewCard"
 import ReviewModal from "./ReviewModal"
 import Search from "./Search"
+import ProductDetailNav from "./ProductDetailNav"
 export default async function ReviewSection() {
     return(
         <div className='border h-[20vh] md:h-[40vh] flex flex-col justify-center items-center'>
             Review Section
+            <ProductDetailNav/>
             <ReviewCard/>
             <ReviewModal/>
         </div>
