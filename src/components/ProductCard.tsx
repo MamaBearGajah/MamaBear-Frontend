90063c03f466e5968479369dc74cb903be1f83c6
Vincent <vincentkiathadi@gmail.com>
2026-05-08 11:57:28 +0800

    add: key benefit

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -46,6 +46,17 @@ export default function ProductCard({
                   )
                 })
               }
+              <br></br>
+              <div className='grid grid-cols-2 grid-rows-3 mt-3 border bg-gray-100 rounded-lg p-4'>
+                <div className='col-start-1 col-end-3 row-start-1 row-end-2 text-[var(--mamabear-dark-pink)]'>KEY BENEFIT</div>
+                <div className='col-start-1 col-end-2 row-start-2 row-end-3'>Affordable Items</div>
+                <div className='col-start-2 col-end-3 row-start-2 row-end-3'>High Quality</div>
+                <div className='col-start-1 col-end-2 row-start-3 row-end-4'>Many Variants</div>
+                <div className='col-start-2 col-end-3 row-start-3 row-end-4'>Nutritious</div>
+              </div>
+              <div className='flex justify-start items-center'>
+                <img src='/package-svgrepo-com.svg' className='w-[20px]'></img>{mockProducts[0].weight} gram (15 sacks)
+              </div>
             </div>
             <div>Add To Cart</div>
         </div>
