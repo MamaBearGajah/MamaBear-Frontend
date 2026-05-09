f0d93db5059a75af434871b3a88f3d328e2c7d92
Vincent <vincentkiathadi@gmail.com>
2026-05-09 08:59:40 +0800

    add: navProductDetail

--- src/components/ProductDetailNav.tsx
+++ src/components/ProductDetailNav.tsx
@@ -1,12 +1,37 @@
 "use client"
 
-export default function ProductDetailNav(){
+import {useState,useEffect, React} from 'react';
+
+type Props = {
+    setParentNavValue: (value: string) => void
+}
+
+export default function ProductDetailNav({setParentNavValue}: Props){
+    const [navValue, setnavValue] = useState('Description')
+    const handleClick = (value: string) => {
+        setnavValue(value)
+        setParentNavValue(value)
+    }
     return(
-    <div className='flex justify-start items-center gap-3'>
-        <p> Description</p>
-        <p> Ingredients</p>
-        <p>How To Use</p>
-        <p>Review</p>
+    <div>
+        <div className='flex justify-start items-center gap-3'>
+                <p onClick={() => handleClick("Description")}>
+                    Description
+                </p>
+
+                <p onClick={() => handleClick("Ingredients")}>
+                    Ingredients
+                </p>
+
+                <p onClick={() => handleClick("How To Use")}>
+                    How To Use
+                </p>
+
+                <p onClick={() => handleClick("Review")}>
+                    Review
+                </p>
+        </div>
+        <hr></hr>
     </div>
     )
 }
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,8 +1,35 @@
-export default function ReviewCard() {
+"use client"
+import {useState, React} from 'react';
 
-  return (
-    <div className="bg-white border border-zinc-300 overflow-hidden rounded transition-transform duration-200 cursor-pointer hover:-translate-y-1 hover:shadow-lg">
-        Review Card
-    </div>
-  );
+export default function ReviewCard({navValue}:{navValue:string}) {
+  switch (navValue){
+    case "Description":
+        return(<div>
+            Description
+            This is the description
+        </div>)
+       
+
+    case "Ingredients":
+        return(<div>
+            Ingredients
+            This is the ingredients
+        </div>)
+        
+    case "How To Use":
+        return(<div>
+            How To Use
+            This is the How To Use
+        </div>)
+        
+      case "Review":
+        return(<div>
+            Review
+            This is the Review
+        </div>)
+        
+      default:
+          return <p>No Content</p>
+  }
+
 }
