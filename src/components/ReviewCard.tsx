4ace856257a9e97c8b3cd83a3f37c9b4cb73bae3
Vincent <vincentkiathadi@gmail.com>
2026-05-09 20:17:53 +0800

    add: review change underline

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,30 +1,36 @@
 "use client"
 import {useState, React} from 'react';
+import { mockProducts } from '../../lib/MockProducts';
 
-export default function ReviewCard({navValue}:{navValue:string}) {
+export default function ReviewCard({navValue, productId}:{navValue:string, productId:number}) {
   switch (navValue){
     case "Description":
-        return(<div>
-            Description
-            This is the description
+        return(
+        <div className='flex flex-col justify-start items-center'>
+            <h2 className='font-bold'>{mockProducts[productId].name}</h2>
+          
+            {mockProducts[productId].description}
         </div>)
        
 
     case "Ingredients":
-        return(<div>
-            Ingredients
-            This is the ingredients
+        return(
+        <div className=''>
+            
+        {mockProducts[productId].ingredients}
         </div>)
         
     case "How To Use":
-        return(<div>
-            How To Use
+        return(
+        <div className='flex flex-col justify-start items-center'>
+            
             This is the How To Use
         </div>)
         
       case "Review":
-        return(<div>
-            Review
+        return(
+        <div className='flex flex-col justify-start items-center'>
+      
             This is the Review
         </div>)
         
