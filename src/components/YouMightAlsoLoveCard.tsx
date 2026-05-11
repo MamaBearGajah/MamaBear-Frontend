e41fc00a6b07647e609d2ce8af85edb64bc1f05d
Vincent <vincentkiathadi@gmail.com>
2026-05-11 10:40:53 +0800

    add: you might also love

---
+++ src/components/YouMightAlsoLoveCard.tsx
@@ -0,0 +1,25 @@
+import {React} from 'react';
+
+type Product = {
+    name:string;
+    rating:number;
+    price:number;
+    imageUrl: string;
+    stock:number;
+    createdAt:Date;
+
+}
+
+
+const YouMightAlsoLoveCard = ({product}:{product:Product}) => {
+  return (
+    <div className='borded rounded-full flex flex-col justify-center items-center md:w-[30%]'>
+        <img className='w-full' src={product.imageUrl}></img>
+        <h2>{product.name}</h2>
+        <p>{product.rating}</p>
+        <p>$ {product.price}</p>
+    </div>
+  );
+};
+
+export default YouMightAlsoLoveCard;
