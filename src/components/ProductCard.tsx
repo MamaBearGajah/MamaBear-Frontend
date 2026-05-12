ab3604dd1b946d214b2d3687d81215e9aa2a91b5
Vincent <vincentkiathadi@gmail.com>
2026-05-12 10:46:37 +0800

    add: breadcrumb

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,6 +6,14 @@ import { mockProducts } from "../../lib/MockProducts";
 import ProductCarousel from "./ProductCarousel";
 import AddToCartQuantity from "./AddToCartQuantity";
 import Stars from "./Stars";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
 export default function ProductCard({
   productId,
@@ -16,16 +24,42 @@ export default function ProductCard({
   const imageArray = mockProducts[0].images.map((item) => item.imageUrl)
   // const fetchedProduct = await getProductId(productId);
   return (
-    <div className="bg-white border md:flex md:items-start rounded transition-transform duration-200">
+    <div>
+        <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/product">Components</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{mockProducts[0].category.name}</BreadcrumbPage>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{mockProducts[0].name}</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <br></br>
+          <hr></hr>
+          <br></br>
+
+    <div className="bg-white md:flex md:items-start rounded transition-transform duration-200">
+
         <div>
             {/* <img src={fetchedProduct.images[0]}   className="w-[400px]"/> */}
             <ProductCarousel images={imageArray}/>
             {/* <img src={mockProducts[0].images[0].imageUrl}   className="w-[400px]"/> */}
         </div>
-        <div className='p-2 md:pl-5 rounded-md border w-[90%] flex flex-col gap-2'>
+        <div className='p-2 md:pl-5 rounded-md w-[90%] flex flex-col gap-2'>
             {/* <h2>{fetchedProduct.title}</h2>
             <p>{fetchedProduct.price}</p>
             <p>{fetchedProduct.description}</p> */}
+
             <h4 className='text-xs text-[var(--mamabear-dark-pink)]'>{mockProducts[0].category.name}</h4>
             <h2 className='text-2xl font-bold'>{mockProducts[0].name}</h2>
             <div className='flex justify-start items-center'> <Stars rating={mockProducts[0].rating}/>{mockProducts[0].bestseller ? (<span className='bg-pink-400 rounded-lg p-2 ml-2'>🏆Bestseller</span>) : null}</div>
@@ -44,6 +78,7 @@ export default function ProductCard({
                     className={
                       `rounded-lg p-2 ml-2 border 
                           ${selected===item.value ? "bg-[var(--mamabear-dark-pink)] text-white" : "bg-white text-black"}
+                          cursor-pointer
                       `}>
                         {item.value}
                     </span>
@@ -66,6 +101,7 @@ export default function ProductCard({
             <AddToCartQuantity price={mockProducts[0].price}/>
         </div>
 
+    </div>
     </div>
   );
 }
--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,5 +1,13 @@
 import ProductCard from "./ProductCard"
 import ProductModal from "./ProductModal"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
 export default async function ProductSection({ productId }: { productId: number }) {
     return(
