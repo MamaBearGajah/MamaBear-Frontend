f624ba10a6cb16ca06c67ae6af2c6c31ce334211
Vincent <vincentkiathadi@gmail.com>
2026-05-14 07:26:16 +0800

    create: benefit component

--- services/index.ts
+++ services/index.ts
@@ -46,16 +46,6 @@ export const fetchProductSlug = async (slug: string): Promise<Product> => {
   }
 };
 
-export const fetchProductVariantId = async (id:string): Promise<ProductVariant[]> => {
-  try{
-    const response = await axios.get(`${BASE_URL}/Products/${id}/variants`)
-    return response.data;
-  }catch(error){
-    console.error("Error fetching product variant:", error);
-    throw new Error("Failed to fetch product variant");
-
-  }
-}
 
 
 export const CreateProduct = async (product: Product): Promise<Product> => {
@@ -95,4 +85,23 @@ export const DeleteProduct = async (id: string): Promise<void> => {
 
 
 
+export const fetchProductVariantId = async (id:string): Promise<ProductVariant[]> => {
+  try{
+    const response = await axios.get(`${BASE_URL}/Products/${id}/variants`)
+    return response.data;
+  }catch(error){
+    console.error("Error fetching product variant:", error);
+    throw new Error("Failed to fetch product variant");
 
+  }
+}
+
+export const CreateProductVariant = async (id: string, productVariant: ProductVariant): Promise<ProductVariant> => {
+  try {
+    const response = await axios.post<ProductVariant>(`${BASE_URL}/Products/${id}/variants`, productVariant);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating product variant:", error);
+    throw new Error("Failed to create product variant");
+  }
+};
