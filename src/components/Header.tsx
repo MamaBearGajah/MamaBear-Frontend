b3f2492093f03d8c8676ea425cb13f1ccd70a3c0
Vincent <vincentkiathadi@gmail.com>
2026-05-15 19:04:17 +0800

    first commit dev2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
         <Link href='/about'>About</Link>
         <Link href='/product'>Product</Link>
         <Link href='/consultation'>Consultation</Link>
-        <Link href='/auth/Login'>Login</Link>
-        <Link href='/auth/Register'>Register</Link>
+        <Link href='/login'>Login</Link>
+        <Link href='/register'>Register</Link>
         <Link href='/auth/admin'>Admin</Link>
       </div>
       
