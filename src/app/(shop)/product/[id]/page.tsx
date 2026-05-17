4aadb4cbd7efdaea0752c706a61309a024b49a18
triyoga884 <triyoga884@gmail.com>
2026-05-17 21:30:20 +0700

    build: add sonner dependency, fix imports and clean up code
    - add sonner toast notification package to project dependencies
    - fix import paths for api client and type definitions to use proper @/ aliases
    - refactor product detail page component formatting and fix broken return logic
    - clean up global css formatting, fix duplicate tailwind import and indentation issues

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -1,32 +1,37 @@
-import ProductSection from "@/components/ProductSection";
-import ReviewSection from "@/components/ReviewSection";
+import ProductSection from "@/components/productDetail/ProductSection";
+import ReviewSection from "@/components/productDetail/ReviewSection";
 import FilterSection from "@/components/FilterSection";
 
-export default async function ProductDetailPage({params,}: {
-  params: Promise<{ id: string }>;}) {
-	const { id } = await params;
-	const productId = Number(id);
+export default async function ProductDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const productId = Number(id);
 
-	// const products : Product = await fetchProduct(productId)
+  // const products : Product = await fetchProduct(productId)
 
-	if (isNaN(productId)) {
-		return 
-		<div>Product Id Not Found</div>
-	}
-	return (
-		<div className="mx-auto w-full xl:w-[1280px] flex flex-col justify-center gap-2 px-10 lg:px-30">
-			{/* ^^^ div to be deleted */}
+  if (isNaN(productId)) {
+    return;
+    <div>Product Id Not Found</div>;
+  }
+  return (
+    <div className="mx-auto flex w-full flex-col justify-center gap-2 px-10 lg:px-30 xl:w-[1280px]">
+      {/* ^^^ div to be deleted */}
 
-			<div className="w-full flex justify-center border ">
-				<div className='w-full md:w-[20%]'><FilterSection productId={productId}/></div>
-				<div className='w-full md:w-[70%]'><ProductSection productId={productId}/>
-			</div></div>
-			<div className="w-full">
-				<ReviewSection/>
-			</div>
-			<div className="block lg:hidden">
-				{/* <AddToCartMobile /> */}
-			</div>
-		</div>
-	);
+      <div className="flex w-full justify-center border">
+        <div className="w-full md:w-[20%]">
+          <FilterSection productId={productId} />
+        </div>
+        <div className="w-full md:w-[70%]">
+          <ProductSection productId={productId} />
+        </div>
+      </div>
+      <div className="w-full">
+        <ReviewSection />
+      </div>
+      <div className="block lg:hidden">{/* <AddToCartMobile /> */}</div>
+    </div>
+  );
 }
