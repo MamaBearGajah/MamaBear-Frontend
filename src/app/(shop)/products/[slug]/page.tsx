4923e2567522391dedb403646c64bbd47f7097b6
Vincent <vincentkiathadi@gmail.com>
2026-05-25 10:34:37 +0800

    add: update price clicking image

--- src/app/(shop)/products/[slug]/page.tsx
+++ src/app/(shop)/products/[slug]/page.tsx
@@ -45,7 +45,7 @@ export default async function ProductDetailPage({
     const isTop5BestsellerFlag = isTop5Bestseller(fetchedAllDataData, productId);
     
     return (
-        <div className="mx-auto w-full xl:w-[100%] md:flex md:flex-col justify-center gap-2 px-5 lg:px-20">
+        <div className="mx-auto w-full xl:w-[100%] md:flex md:flex-col justify-center gap-2 px-5 lg:px-20 bg-[var(--background)]">
             <div className="w-full block md:flex md:justify-center">
                 <div className='w-full md:w-[100%] top-2'><ProductSection productId={productId} product={fetchedDataData}  productVariant={productVariant} isTop5BestsellerFlag={isTop5BestsellerFlag}/></div>
             </div>
