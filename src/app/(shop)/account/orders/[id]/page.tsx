5e217d5ebfd1965680658e135f68cde00c36fc48
Vincent <vincentkiathadi@gmail.com>
2026-06-02 00:26:34 +0800

    add: add review after delivered

--- src/app/(shop)/account/orders/[id]/page.tsx
+++ src/app/(shop)/account/orders/[id]/page.tsx
@@ -88,7 +88,7 @@ async function fetchOrder(id: string): Promise<OrderType> {
   }
 
   const localOrder = await getOrderById(id);
-  return localOrder ?? MOCK_ORDERS.find((order) => order.id === id) ?? MOCK_ORDERS[0];
+  return localOrder ?? MOCK_ORDERS.find((order) => order.id === id) ?? MOCK_ORDERS[3];
 }
 
 function formatCurrency(value: number) {
