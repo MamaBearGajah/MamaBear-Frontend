4d742a63227a56f28712b101aef782e36d9b7a56
Vincent <vincentkiathadi@gmail.com>
2026-06-02 10:46:57 +0800

    fix: variant imageUrl

--- src/components/productDetail/YouMightAlsoLoveCard.tsx
+++ src/components/productDetail/YouMightAlsoLoveCard.tsx
@@ -20,7 +20,7 @@ const YouMightAlsoLoveCard = ({product}:{product:Product}) => {
   return (
     <div className='border rounded-lg flex flex-col justify-start p-5 items-start w-[100%] md:w-[80%] hover:shadow-lg transition duration-300 cursor-pointer hover:scale-102'>
       <Link href={`/products/${product.slug}`} className='w-[60%]'>
-        <img className='w-[full]'  src="/Logo Mamabear.png"></img>
+        <img className='w-[full]'  src={product.imageUrl} alt={product.name}></img>
         <h2 className='mt-2 mb-2 font-bold text-xs md:text-lg'>{product.name}</h2>
         <div className='flex justify-start items-center mt-2 mb-2 md:text-lg text-xs'><Stars rating={3} /><span className='ml-3 md:text-lg text-xs'>{product.rating}</span></div>
         {/* <p>{product.rating}</p> */}
